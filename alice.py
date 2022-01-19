1d621972606444fa4cc580edabb35c4ee4541578
Igor Korostil <eeghor@gmail.com>
2022-01-19 23:39:34 +1100

    hae

--- alice.py
+++ alice.py
@@ -38,7 +38,7 @@ class WordMapper:
                      'without', 'would', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', '‘d', '‘ll', '‘m', '‘re', 
                      '‘s', '‘ve', '’d', '’ll', '’m', '’re', '’s', '’ve'}
 
- someficujj = defaultdict(int)
+ someficujj2 = defaultdict(int)
 
 def get_alice():
     
